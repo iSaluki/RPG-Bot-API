b7704662350bb0cf125869ef2e243d3c165df340
Seth <seth@salukicorporation.com>
2021-04-15 10:57:18 +0100

    Basic GET and POST requests

--- api.py
+++ api.py
@@ -1,1 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def test():
+    return 'Basic API call'
+
+@app.route('/post', methods=["POST"])
+def testpost():
+     input_json = request.get_json(force=True) 
+     dictToReturn = {'text':input_json['text']}
+     return jsonify(dictToReturn)
+
+app.run()
