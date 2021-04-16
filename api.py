56368cc5424e11906ebed8ff51c4633ae39bb11c
Seth <seth@salukicorporation.com>
2021-04-16 18:38:05 +0100

    Database changes

--- api.py
+++ api.py
@@ -15,22 +15,65 @@ db = client["rpg-db"]
 
 
 @app.route('/get')
-def test():
-    return 'You are in a forest surrounded by wolves. I hope you picked up the flaming torch?'
+def get():
+    user_id = int(request.args.get("user"))
+    print(user_id)
+    #user_id = int(input_json["user"])
+    collection = db["users"]
+    for user in collection.find({"user_id":user_id}):
+        print(user["user_id"], user["map"])
+        _map = user["map"]
+        loc = user["location"]
+    collection = db[_map]
+    for loc in collection.find({"location_id":loc}):
+        print(loc["name"],loc["description"])
+        
+    return loc["description"]
 
 @app.route('/post', methods=["POST"])
 def testpost():
-     input_json = request.get_json(force=True) 
-     user_id = int(input_json["user"])
-     collection = db["users"]
-     for user in collection.find({"user_id":user_id}):
-         print(user["user_id"], user["map"])
-     dictToSend = {"response":"OK","command":input_json["command"],"args":input_json["args"]}
-     return jsonify(dictToSend)
-     #dictToReturn = {'text':input_json['text']}
-     #print("Recieved data:")
-     #print(dictToReturn)
-     #return jsonify(dictToReturn)
+    input_json = request.get_json(force=True) 
+    user_id = int(input_json["user"])
+    collection = db["users"]
+    for user in collection.find({"user_id":user_id}):
+        print(user["user_id"], user["map"])
+        _map = user["map"]
+        loc = user["location"]
+        #d = user["_id"]
+
+    if loc == 1:
+        loc = 2
+    else:
+        loc = 1     
+    new_val = {"location": loc}
+    result = collection.update_one(
+        {"user_id" : user_id},
+        {"$set": new_val},
+        upsert=True)
+ 
+
+    dictToSend = {"response":"OK","command":input_json["command"],"args":input_json["args"]}
+    return jsonify(dictToSend)
+    #dictToReturn = {'text':input_json['text']}
+    #print("Recieved data:")
+    #print(dictToReturn)
+    #return jsonify(dictToReturn)
+
+
+
+#def move(user_id,moveTo):
+#    input_json = request.get_json(force=True) 
+#    user_id = int(input_json["user"])
+#    collection = db["users"]
+
+#    for user in collection.find({"user_id":user_id}):
+#         loc = user["location"]
+#       collection = db["map_tutorial"]
+#    for locations in collection.find({"location_id":loc}):
+#        allowedMove = location.linksTo
+#    if moveTo is in allowedMove:
+#            update db to moveTo
+#            respond to discord and say the user has moved  
 
 
 
