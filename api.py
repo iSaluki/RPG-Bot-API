7303f60de593ba0c02f827168ac3409557f93c90
Seth <seth@salukicorporation.com>
2021-04-16 17:51:10 +0100

    Login to the database

--- api.py
+++ api.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, jsonify
 import json
+from pymongo import MongoClient
+import urllib
+
 app = Flask(__name__)
+
+#client = MongoClient("mongodb+srv://mb:"+urllib.parse.quote_plus("@ntholog3y")+"@cluster0.zl2rh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+#db = client["contacts"]
+#collection = db["people"]
+
+client = MongoClient("mongodb+srv://discord:"+urllib.parse.quote_plus("79wXglvmonJBwVK0")+"@rpg-data.avgt0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client["rpg-db"]
 
 
 @app.route('/get')
@@ -10,7 +20,11 @@ def test():
 @app.route('/post', methods=["POST"])
 def testpost():
      input_json = request.get_json(force=True) 
-     print(input_json)
+     user_id = int(input_json["user"])
+     collection = db["users"]
+     user = collection.find({"user_id":user_id})
+
+     print(user)
      dictToSend = {"response":"OK","command":input_json["command"],"args":input_json["args"]}
      return jsonify(dictToSend)
      #dictToReturn = {'text':input_json['text']}
