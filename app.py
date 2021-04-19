64a2543b9d8afbe6b41d61102fd2d1a77bbaa3ca
Seth <seth@salukicorporation.com>
2021-04-19 15:05:50 +0100

    Arg attempt

--- app.py
+++ app.py
@@ -84,8 +84,8 @@ def testpost():
     try:
         args = user_request["args"]
     except:
-        args = ""
-        
+        args = user_request[""]
+
     if command == "buy":
         reply = "This has yet to be implented!"
     elif command == "drop":
