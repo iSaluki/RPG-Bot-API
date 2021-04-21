7cf237f84350cb0c40bdb4daaf8f9403491ab33d
Seth <seth@salukicorporation.com>
2021-04-21 11:08:50 +0100

    Authentication Basics
    Not working, just testing some things out

--- app.py
+++ app.py
@@ -12,8 +12,18 @@ client = MongoClient("mongodb://api:ASrBP1PUB6RUwlpk@rpg-data-shard-00-00.avgt0.
 db = client["rpg-db"]
 PRODUCTION = True
 
+authToken = "eyJhbGciOiJQUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.MqF1AKsJkijKnfqEI3VA1OnzAL2S4eIpAuievMgD3tEFyFMU67gCbg-fxsc5dLrxNwdZEXs9h0kkicJZ70mp6p5vdv-j2ycDKBWg05Un4OhEl7lYcdIsCsB8QUPmstF-lQWnNqnq3wra1GynJrOXDL27qIaJnnQKlXuayFntBF0j-82jpuVdMaSXvk3OGaOM-7rCRsBcSPmocaAO-uWJEGPw_OWVaC5RRdWDroPi4YL4lTkDEC-KEvVkqCnFm_40C-T_siXquh5FVbpJjb3W2_YvcqfDRj44TsRrpVhk6ohsHMNeUad_cxnFnpolIKnaXq_COv35e9EgeQIPAbgIeg"
+
 # Emojis
 x_emoji = "<:X_:833700097903689728>"
+
+
+def Authenticate(token, headers):
+    receivedToken = request.headers.get('Authentication')
+    if token == receivedToken:
+        return True
+    else:
+        return False
 
 # Database Abstraction
 # A collection of functions to get data from the database and to write to the database
@@ -81,6 +91,7 @@ def get():
 def testpost():
     logging.debug(f"{asctime()} TESTPOST: started")
     user_request = request.get_json(force=True) 
+    print(user_request)
     logging.debug(f"{asctime()} GETPOST: user_request = {user_request}")
     user_id = int(user_request["user"])
     command = user_request["command"].lower()
