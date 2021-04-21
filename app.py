45a96b625dd5e94225fa598c964e00c936f506c1
Seth <seth@salukicorporation.com>
2021-04-21 11:34:56 +0100

    Find Production

--- app.py
+++ app.py
@@ -4,13 +4,25 @@ from pymongo import MongoClient
 import urllib
 from time import asctime
 import logging
+import os
 
 logging.basicConfig(filename="api.log", level=logging.DEBUG)
+
+
+envProd = os.environ.get('PRODUCTION')
+logging.debug(f"{asctime()} ENVIRONMENT: Trying to get PRODUCTION")
+if envProd == True:
+    PRODUCTION = True
+    logging.debug(f"{asctime()} ENVIRONMENT: Found PRODUCTION, set it to True")
+else:
+    PRODUCTION = False
+    logging.debug(f"{asctime()} ENVIRONMENT: Unable to find PRODUCTION, set it to False")
+
+    
 app = Flask(__name__)
 client = MongoClient("mongodb://api:ASrBP1PUB6RUwlpk@rpg-data-shard-00-00.avgt0.mongodb.net:27017,rpg-data-shard-00-01.avgt0.mongodb.net:27017,rpg-data-shard-00-02.avgt0.mongodb.net:27017/rpg-db?ssl=true&replicaSet=atlas-6e05a9-shard-0&authSource=admin&retryWrites=true&w=majority")
 
 db = client["rpg-db"]
-PRODUCTION = True
 
 authToken = "eyJhbGciOiJQUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.MqF1AKsJkijKnfqEI3VA1OnzAL2S4eIpAuievMgD3tEFyFMU67gCbg-fxsc5dLrxNwdZEXs9h0kkicJZ70mp6p5vdv-j2ycDKBWg05Un4OhEl7lYcdIsCsB8QUPmstF-lQWnNqnq3wra1GynJrOXDL27qIaJnnQKlXuayFntBF0j-82jpuVdMaSXvk3OGaOM-7rCRsBcSPmocaAO-uWJEGPw_OWVaC5RRdWDroPi4YL4lTkDEC-KEvVkqCnFm_40C-T_siXquh5FVbpJjb3W2_YvcqfDRj44TsRrpVhk6ohsHMNeUad_cxnFnpolIKnaXq_COv35e9EgeQIPAbgIeg"
 
