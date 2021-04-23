373101ab205c2f265e9d228a35544e2d967821ba
Mr Brant <mb@princehenrys.worcs.sch.uk>
2021-04-23 14:07:33 +0100

    Update testGetItems.py
    Added new GetPlayerItemsAtLocationFunction

--- testGetItems.py
+++ testGetItems.py
@@ -22,7 +22,7 @@ def GetInventory(user_id):
     inv = []
     for user_item in user_items.find({"user_id":user_id, "status":"inventory"}):
         for item in items.find({"item_id":user_item["item_id"]}):
-            inv.append({"item_id":item["item_id"], "description":item["description"], "gettable":item["gettable"], "universal":item["universal"]})
+            inv.append({"item_id":item["item_id"], "description":item["description"], "emoji":item["emoji"], "gettable":item["gettable"], "universal":item["universal"]})
     return inv
 
 def GetInventoryDescriptions(user_id):
@@ -42,7 +42,7 @@ def GetPlayerItemsAtLocation(user_id, _map, location):
     user_items_here = []
     for user_item in user_items.find({"user_id":user_id, "status":"dropped", "map_name":_map, "location_id":location}):
         for item in items.find({"item_id":user_item["item_id"]}):
-            user_items_here.append({"item_id":item["item_id"], "description":item["description"], "gettable":item["gettable"], "universal":item["universal"]})
+            user_items_here.append({"item_id":item["item_id"], "description":item["description"], "emoji":item["emoji"], "gettable":item["gettable"], "universal":item["universal"]})
     return user_items_here
 
 def GetDefaultItemsAtLocation(_map, location):
@@ -50,45 +50,30 @@ def GetDefaultItemsAtLocation(_map, location):
     items = db["items"]
     default_items_here = []
     for item in items.find({"map_name":_map, "location_id":location}):
-        default_items_here.append({"item_id":item["item_id"], "description":item["description"], "gettable":item["gettable"], "universal":item["universal"]})
+        default_items_here.append({"item_id":item["item_id"], "description":item["description"], "emoji":item["emoji"], "gettable":item["gettable"], "universal":item["universal"]})
     return default_items_here
 
-def GetItems(user_id, _map, location):
-    logging.debug(f"{asctime()} GETITEMS: passed in user_id = {user_id}, map_name = {_map}, location_id = {location}")
-    user = GetUser(user_id)
-    logging.info(f"User = {user}")
-    collection = db["items"]
+def GetItemsForUserAtLocation(user_id, _map, location):
+    logging.debug(f"{asctime()} GETITEMSFORUSERATLOCATION: passed in user_id = {user_id}, map_name = {_map}, location_id = {location}")
+    
+    default_items = GetDefaultItemsAtLocation(_map, location)
+    logging.debug(f"{asctime()} GETITEMSFORUSERATLOCATION: default_items = {default_items}")
+    user_items = GetPlayerItemsAtLocation(user_id, _map, location)
+    logging.debug(f"{asctime()} GETITEMSFORUSERATLOCATION: user_items = {user_items}")
+    inventory = GetInventory(user_id)
+    logging.debug(f"{asctime()} GETITEMSFORUSERATLOCATION: inventory = {inventory}")
 
-    # Get all the items in the user's inventory
-    inv = GetInventory(user_id)
+    for item in default_items:
+        if item not in user_items and item not in inventory:
+            user_items.append(item)
+    
+    return user_items
 
-    #Find all the items that default to the current location
-    items = {}
-    for item in collection.find({"default_map":user["map_name"], "default_location":user["location_id"]}):
-        if item["universal"]:
-            items["item_id"] = item["item_id"]
-            items["description"] = item["description"]
-            items["gettable"] = item["gettable"]
-    logging.info(f"Items found in location {items}")
-
-    #Only keep the ones that the user hasn't already interacted with
-    keep = {}
-    for item in items:
-        if item["item_id"] not in user["inventory"]:
-            keep["item_id"] = item["item_id"]
-            keep["description"] = item["description"]
-            keep["gettable"] = item["gettable"]
-    logging.info(f"Items being kept {keep}")
-
-    # Add any items that the user has previously left here
-    for user_items in user["inventory"]:
-        if user_items["map_name"] == user["map_name"] and user_items["location_id"] == user["location_id"]:
-            keep["item_id"] = user_items["item_id"]
-            for item in collection.find({"item_id":user_items["user_id"]}):
-                keep["description"] = item["description"]
-                keep["gettable"] = item["gettable"]
-    logging.info(f"Items being kept plus those previously left by the user {keep}")
-    return keep
-
-print(GetInventory(183240527649570816))
+#print(GetInventory(183240527649570816))
 #print(GetItems(183240527649570816, "map_tutorial", 72))
+'''
+User Ids: 
+183240527649570816  Seth
+595321346498625536
+834853116003745792
+'''
